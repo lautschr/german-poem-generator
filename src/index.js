cef346be0f10fc893b0d071421bde2f2efb37f29
lautschr <144703338+lautschr@users.noreply.github.com>
2025-07-03 10:28:27 +0200

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
-    new Typewriter("#poem", {
-      strings: "Mit einem Dach und seinem Schatten dreht",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "45eb40051403b9b0bo97e7f72b2a5t17";
+  let context =
+    "You are a romantic Poem expert and love to write short poems in German. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
+  let prompt = `User instructions: Generate a German poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
